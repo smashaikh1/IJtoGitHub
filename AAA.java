d7915203f3491395fb0b1f764f1bb36d76b4d9e3
smashaikh1 <106754371+smashaikh1@users.noreply.github.com>
2022-07-02 11:29:23 +0530

    Create AAA.java
    add file from GitHub

---
+++ AAA.java
@@ -0,0 +1,8 @@
+public class AAA
+{
+    public static void main(String args[])
+    {
+        System.out.println("This is simple project link from from GitLab to IJ IDE..by sk md..ABC1 class");
+        System.out.println("AAAAAAAAA.......");
+    }
+}
